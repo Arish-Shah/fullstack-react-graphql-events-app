a923084ec45fc15021c6df5a60773b449018e1d2
Arish Shah <arish0246@gmail.com>
2021-01-24 10:51:16 +0530

    add cors

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv").config();
 
 const schema = require("./graphql/schema");
@@ -10,6 +11,8 @@ const isAuth = require("./middlewares/isAuth");
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use(isAuth);
 
